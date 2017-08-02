95ded92b1bb1096d0f561f513a98c05c7a8b0c49
Shyam Gupta <postboxshyam@gmail.com>
2017-08-02 19:20:42 -0400

    Final draft of the assignment

--- typelist.py
+++ typelist.py
@@ -1,12 +1,31 @@
-def findType(userInput):
-	if(isinstance(userInput,int)):
-		if(userInput>100):print "That's a big number!"
-		else: print "That's a small number"
-	elif (isinstance(userInput,str)):
-		if(len(userInput)>=50):print "Long sentence"
-		else: print "Short Sentence"
+'''
+	Date: Aug-2-2017
+	Author: Shyam Gupta
+	Program accepts a list comprising of strings & integers.
+	Based on the type of each list element, it will print the concatenated string and the sum of integers
+'''
+
+def typeList(userInput):
+	total = 0
+	astring = ''
+	int_type = 0
+	str_type=0
+	for i in userInput:
+		if(isinstance(i,int)):
+			int_type += 1
+			total += i
+		else:
+			str_type += 1
+			astring += ' ' + i
+	if int_type>0 and str_type>0:
+		print 'The list you entered is of mixed type'
+		print 'String: '+ astring
+		print 'Sum: ',total
+	elif int_type==0:
+		print 'The list you entered is of string type'
+		print 'String: '+ astring
 	else:
-		if(len(userInput)>=10):print "Big List"
-		else: print "Short List"
-userInput = input('Enter an integer, string or a List: ')
-findType(userInput)
+		print 'The list you entered is of integer type'
+		print 'Sum: ' , total
+userInput = input('Enter the list: ')
+typeList(userInput)
