ced9417733a1eb6d24298b10bed09f646760032f
Shyam Gupta <postboxshyam@gmail.com>
2017-08-04 21:36:16 -0400

    Scores and Grades

--- scoresAndGrades.py
+++ scoresAndGrades.py
@@ -1,16 +1,15 @@
 import random
-while(True):
-	score = input('Enter score or type quit to exit:  ')
-	if(str(score).lower() == 'quit'):
-		break
-	else:
+def scores():
+	print 'Scores and Grades'
+	for i in range(10):
+		score = random.randint(60,100)
 		if(score>=60 and score<70):
-			print 'Grade - D'
+			print 'Score: ',score,';','Your grade is D'
 		elif(score>=70 and score<80):
-			print 'Grade - C'
+			print 'Score: ',score,';','Your grade is C'
 		elif(score>=80 and score<90):
-			print 'Grade - B'
-		elif (score>=90 and score<=100):
-			print 'Grade - A'
+			print 'Score: ',score,';','Your grade is B'
 		else:
-			print 'Enter a valid input'
+			print 'Score: ',score,';','Your grade is A'
+	print 'End of the program. Bye!'
+scores()
