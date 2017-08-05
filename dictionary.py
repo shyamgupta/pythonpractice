f0fa28d818e0bb58df4c54c8d4d34e5e5e29a03c
Shyam Gupta <postboxshyam@gmail.com>
2017-08-04 23:09:18 -0400

    Making and Reading from Dictionaries"

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+def printDictionary(dictionary):
+	for key in dictionary:
+		print 'My ',key,' is ',dictionary[key]
+
+
+
+person = {
+	
+	'name': "Anna",
+	'Age': 101,
+	'Country of Birth': "United States",
+	'favorite language': "Python"
+	
+}
+
+printDictionary(person)
+
