c9fc6c6445505af294fdfe516d229d2fb05f76ce
Shyam Gupta <postboxshyam@gmail.com>
2017-08-06 10:17:07 -0400

    dict to Tuples

---
+++ dictTuples.py
@@ -0,0 +1,11 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+
+def dictTuples(a_dictionary):
+	print a_dictionary.items()
+
+dictTuples(my_dict)
